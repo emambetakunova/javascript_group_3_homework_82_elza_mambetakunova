16995a31e144ee5d3140df62a6e4a14cb9694a24
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-08 13:44:37 +0600

    add app

---
+++ app/tracks.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const Track = require('../models/Track');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    if (req.query.album) {
+        Track.find({album: req.query.album})
+            .then(result => {
+                if (result) return res.send(result);
+                res.sendStatus(404)
+            })
+            .catch(() => res.sendStatus(500));
+    } else {
+        Track.find()
+            .then(tracks => {
+                res.send(tracks)
+                    .catch(() => res.sendStatus(500))
+            })
+    }
+});
+
+router.get('/:id', (req, res) => {
+    Track.findById(req.params.id)
+        .then(result => {
+            if (result) return res.send(result);
+            res.sendStatus(404)
+        })
+        .catch(() => res.sendStatus(500));
+});
+
+router.post('/', (req, res) => {
+    const trackData = req.body;
+
+    const track = new Track(trackData);
+    track.save()
+        .then(result => res.send(result))
+        .catch(error => res.sendStatus(400).send(error));
+});
+
+
+
+
+module.exports = router;
